46374ffbae6c4302ecd3a2604a24d07481e600b8
bendaikh <bendaikh2000@gmail.com>
2026-06-10 21:45:52 +0100

    commited

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Shared hosting entry point when the web root is the project directory.
+ * Delegates to the Laravel front controller in public/.
+ */
+
+require __DIR__.'/public/index.php';
